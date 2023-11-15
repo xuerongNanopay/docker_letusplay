5482e3116b46c94d7a464e1c201cb51098b54f8f
xuerongNanopay <xuerong@nanopay.net>
2023-11-14 19:07:48 -0500

    add small project

--- lession01/app.js
+++ lession01/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 
-// import connectToDatabase from './helper'
+import connectToDatabase from './helpers'
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('<h1>Docker Lesson01</h1>')
 })
+
+await connectToDatabase()
 
 app.listen(3000)
---
+++ lession01/helpers.js
@@ -0,0 +1,9 @@
+const connectToDatabase = () => {
+  return dummyPromist = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, 1000)
+  })
+}
+
+export default connectToDatabase
