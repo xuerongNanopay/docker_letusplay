bec4bc5aea7586a6770e8ce4a7395e66fa8e1dda
xuerongNanopay <xuerong@nanopay.net>
2023-11-17 16:55:32 -0500

    mv files

---
+++ terraform/vpc.tf
@@ -0,0 +1,81 @@
+resource "aws_vpc" "xrw_ue1_vpc" {
+  cidr_block = "10.0.0.0/16"
+  # enable_dns_hostnames = true
+
+  tags = {
+    Name = "xrw_ue1_vpc"
+  }
+}
+
+resource "aws_default_route_table" "xrw_ue1_vpc_default_rtab" {
+  default_route_table_id = aws_vpc.xrw_ue1_vpc.default_route_table_id
+  tags = {
+    Name = "xrw_ue1_vpc_default_rtab"
+  }
+}
+
+resource "aws_internet_gateway" "xrw_ue1_vpc_igw" {
+  vpc_id = aws_vpc.xrw_ue1_vpc.id
+
+  tags = {
+    Name = "xrw_ue1_vpc_igw"
+  }
+}
+
+# Four public submit
+resource "aws_subnet" "xrw_ue1a_private_snet" {
+  vpc_id = aws_vpc.xrw_ue1_vpc.id
+  cidr_block = "10.0.0.0/20"
+  # map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "xrw_ue1a_private_snet"
+  }
+  
+}
+resource "aws_subnet" "xrw_ue1b_private_snet" {
+  vpc_id = aws_vpc.xrw_ue1_vpc.id
+  cidr_block = "10.0.16.0/20"
+  # map_public_ip_on_launch = true
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "xrw_ue1b_private_snet"
+  }
+}
+resource "aws_subnet" "xrw_ue1c_private_snet" {
+  vpc_id = aws_vpc.xrw_ue1_vpc.id
+  cidr_block = "10.0.32.0/20"
+  # map_public_ip_on_launch = true
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "xrw_ue1c_private_snet"
+  }
+}
+resource "aws_subnet" "xrw_ue1d_private_snet" {
+  vpc_id = aws_vpc.xrw_ue1_vpc.id
+  cidr_block = "10.0.48.0/20"
+  # map_public_ip_on_launch = true
+  availability_zone = "us-east-1d"
+  tags = {
+    Name = "xrw_ue1d_private_snet"
+  }
+}
+
+# Two private submit
+resource "aws_subnet" "xrw_ue1e_public_snet" {
+  vpc_id = aws_vpc.xrw_ue1_vpc.id
+  cidr_block = "10.0.64.0/20"
+  availability_zone = "us-east-1e"
+  tags = {
+    Name = "xrw_ue1e_public_snet"
+  }
+}
+resource "aws_subnet" "xrw_ue1f_public_snet" {
+  vpc_id = aws_vpc.xrw_ue1_vpc.id
+  cidr_block = "10.0.80.0/20"
+  availability_zone = "us-east-1f"
+  tags = {
+    Name = "xrw_ue1f_public_snet"
+  }
+}
