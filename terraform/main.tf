dc56eeaf6dae7c342db5198ade31aab06ab8a011
xuerongNanopay <xuerong@nanopay.net>
2023-11-17 15:20:01 -0500

    add tag for subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "xrw_ue1_vpc" {
   # enable_dns_hostnames = true
 
   tags = {
-    Name = "xrw_us-east-1_vpc"
+    Name = "xrw_ue1"
   }
 }
 
@@ -13,24 +13,38 @@ resource "aws_subnet" "xrw_ue1a_private_snet" {
   cidr_block = "10.0.0.0/20"
   # map_public_ip_on_launch = true
   availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "xrw_ue1a_private"
+  }
+  
 }
 resource "aws_subnet" "xrw_ue1b_private_snet" {
   vpc_id = aws_vpc.xrw_ue1_vpc.id
   cidr_block = "10.0.16.0/20"
   # map_public_ip_on_launch = true
   availability_zone = "us-east-1b"
+  tags = {
+    Name = "xrw_ue1b_private"
+  }
 }
 resource "aws_subnet" "xrw_ue1c_private_snet" {
   vpc_id = aws_vpc.xrw_ue1_vpc.id
   cidr_block = "10.0.32.0/20"
   # map_public_ip_on_launch = true
   availability_zone = "us-east-1c"
+  tags = {
+    Name = "xrw_ue1c_private"
+  }
 }
 resource "aws_subnet" "xrw_ue1d_private_snet" {
   vpc_id = aws_vpc.xrw_ue1_vpc.id
   cidr_block = "10.0.48.0/20"
   # map_public_ip_on_launch = true
   availability_zone = "us-east-1d"
+  tags = {
+    Name = "xrw_ue1d_private"
+  }
 }
 
 # Two private submit
@@ -38,9 +52,15 @@ resource "aws_subnet" "xrw_ue1e_public_snet" {
   vpc_id = aws_vpc.xrw_ue1_vpc.id
   cidr_block = "10.0.64.0/20"
   availability_zone = "us-east-1e"
+  tags = {
+    Name = "xrw_ue1e_public"
+  }
 }
 resource "aws_subnet" "xrw_ue1f_public_snet" {
   vpc_id = aws_vpc.xrw_ue1_vpc.id
   cidr_block = "10.0.80.0/20"
   availability_zone = "us-east-1f"
+  tags = {
+    Name = "xrw_ue1f_public"
+  }
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,6 +6,7 @@ terraform {
   }
 }
 
+# N. Virginia
 provider "aws" {
   region     = "us-east-1"
   shared_credentials_files = [ "~/.aws/credentials" ]
