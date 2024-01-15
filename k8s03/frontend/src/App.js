b2a10255c9f55d0f0c2148389c7f14b0a7371afa
xuerongNanopay <xuerong@nanopay.net>
2024-01-15 17:36:44 -0500

    update

--- k8s03/frontend/src/App.js
+++ k8s03/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const fetchTasks = useCallback(function () {
-    fetch('http://192.168.99.100:32140/tasks', {
+    fetch(`/api/tasks`, {
       headers: {
         'Authorization': 'Bearer abc'
       }
@@ -29,7 +29,7 @@ function App() {
   );
 
   function addTaskHandler(task) {
-    fetch('http://192.168.99.100:32140/tasks', {
+    fetch(`/api/tasks`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
